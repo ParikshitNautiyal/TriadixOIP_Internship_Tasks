491d43129e4bfbe4e45aee0aa53add84d203e001
Parikshit Nautiyal <parikshitnautiyal8@hmail.com>
2026-08-01 23:04:32 +0530

    Initial commit: Week 1 tasks

---
+++ tasks.py
@@ -0,0 +1,19 @@
+'''
+1. Collect Dataset
+        ↓
+2. Load Dataset
+        ↓
+3. Clean & Preprocess
+        ↓
+4. Exploratory Data Analysis (EDA)
+        ↓
+5. Feature Engineering
+        ↓
+6. Train Multiple ML Models
+        ↓
+7. Compare Accuracy
+        ↓
+8. Predict New Student Performance
+        ↓
+9. Find Important Features
+'''
